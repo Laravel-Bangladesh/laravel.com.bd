6179399338d4b94311407872eddd93b71cd201e1
Jubayed Hossain <9925505+jubayed@users.noreply.github.com>
2018-11-04 23:12:25 +0600

    add contributor view

---
+++ resources/views/contributors/layout.blade.php
@@ -0,0 +1,175 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> অবদানকারীর -  লারাভেল - ওয়েব ডেভেলপারদের জন্য পিএইচপি ফ্রেমওয়ার্ক</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="author" content="Jubayed Hossain">
+    <meta name="description" content="লারাভেল - ওয়েব ডেভেলপারদের জন্য পিএইচপি ফ্রেমওয়ার্ক">
+    <meta name="keywords" content="laravel, php, framework, web, artisans, jubayed, bangla, bd, লারাভেল, পিএইচপি, ফ্রেমওয়ার্ক">
+    <meta name="google-site-verification" content="0zGjqFVduurLPHXaZEJB_sCiK5STdxJ_NPR0wwmTc1o" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @if (isset($canonical))
+        <link rel="canonical" href="{{ url($canonical) }}" />
+    @endif
+    <link href='https://fonts.googleapis.com/css?family=Miriam+Libre:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="{{ elixir('assets/css/laravel.css') }}">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/7737514/7707592/css/fonts.css" />
+    <link rel="apple-touch-icon" href="/favicon.png">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.26/vue.min.js"></script>
+</head>
+<body class="@yield('body-class', 'docs') language-php">
+
+    <span class="overlay"></span>
+
+    <nav class="main">
+        <a href="/" class="brand nav-block">
+            {!! svg('laravel-logo') !!}
+            <span>লারাভেল</span>
+        </a>
+
+        <div class="search nav-block invisible">
+            {!! svg('search') !!}
+            <input placeholder="অনুসন্ধান" type="text" v-model="search" id="search-input" v-on:blur="reset" />
+        </div>
+
+        <ul class="main-nav" v-if="! search">
+            @include('partials.main-nav')
+        </ul>
+
+        @if (Request::is('docs*') && isset($currentVersion))
+            @include('partials.switcher')
+        @endif
+
+        <div class="responsive-sidebar-nav">
+            <a href="#" class="toggle-slide menu-link btn">&#9776;</a>
+        </div>
+    </nav>
+
+    @yield('content')
+
+    <footer class="main">
+        <ul>
+            @include('partials.main-nav')
+        </ul>
+ <p>লারভেল Taylor Otwell এর ট্রেডমার্ক। কপিরাইট &copy; Taylor Otwell.</p>
+        <p class="less-significant">
+            <a href="http://jackmcdade.com">
+                Designed by<br>
+                {!! svg('jack-mcdade') !!}
+            </a>
+        </p>
+    </footer>
+
+    <script>
+        var algolia_app_id      = '{{ Config::get('algolia.connections.main.id', false) }}';
+        var algolia_search_key  = '{{ Config::get('algolia.connections.main.search_key', false) }}';
+        var version             = '{{ isset($currentVersion) ? $currentVersion : DEFAULT_VERSION }}';
+    </script>
+
+    @include('partials.algolia_template')
+
+    <script src="{{ elixir('assets/js/laravel.js') }}"></script>
+    <script src="/assets/js/viewport-units-buggyfill.js"></script>
+    <script>window.viewportUnitsBuggyfill.init();</script>
+ <script>
+        var _gaq=[['_setAccount','UA-116029141-1'],['_trackPageview']];
+        (function(d,t){
+            var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)
+        }(document,'script'));
+    </script>
+
+    <script>
+
+        $(document).ready(function(){
+
+
+         var user= [];
+         
+         
+         user = ["9925505", "7860229"];
+
+            // Assign handlers immediately after making the request,
+// and remember the jqxhr object for this request
+$.getJSON( "https://api.github.com/repos/Laravel-Bangladesh/docs/pulls?state=all", function() {
+  console.log( "get all pulls" );
+})
+.done(function(data) {
+    $.each( data, function( key, value ) {
+
+     var a = user.indexOf( ""+ value.user.id + "");
+
+     if (a == -1 ) {
+         user.push("" + value.user.id + "" );
+                //console.log(value.user);
+            }
+
+        });
+
+    build_user(user);
+
+})
+
+.fail(function() {
+    console.log( "error pulls" );
+})
+
+
+
+function build_user(user){
+
+
+
+    var name = "";
+    var location = "&nbsp;&nbsp;";
+    var blog = "#";
+
+    $.each( user, function( key, value ) {
+
+        $.getJSON( "https://api.github.com/user/"+ value +"", function() {
+          console.log( "success" );
+      })
+        .done(function(data) {
+          console.log(data);
+
+  // ===========
+
+  if(data.name == null )
+    name = data.login;
+else
+    name = data.name;
+
+if(data.location != null )
+    location = data.location;
+
+if(data.blog != null )
+    blog = data.blog;
+else
+  blog = "#";
+
+
+$html = ' <div class="profile-box"> <img src="'+ data.avatar_url +'" alt="'+ data.name +'"> <h3>'+ name +'</h3><h4>'+ location +'</h4><div class="btn-container"><a href="'+ data.html_url +'"  target="_blank" class="profile-btn" >Github</a><a href="'+ blog +'"  target="_blank" class="profile-btn"><i class="fa fa-globe" aria-hidden="true"></i>Website</a></div></div>';
+
+$('#contributors_list').append($html);
+
+
+       })
+
+        .fail(function() {
+            console.log( "error con-user-data" );
+        })
+
+
+    });
+}
+
+});
+
+    </script>
+</body>
+
+
+</html>
---
+++ resources/views/contributors/view.blade.php
@@ -0,0 +1,157 @@
+@extends('contributors.layout')
+
+@section('body-class', 'home')
+
+@section('content')
+
+<nav id="slide-menu" class="slide-menu" role="navigation">
+
+    <div class="brand">
+        <a href="/">
+            <img src="/assets/img/laravel-logo-white.png" height="50" alt="Laravel white logo">
+        </a>
+    </div>
+
+    <ul class="slide-main-nav">
+        @include('partials.main-nav')
+    </ul>
+
+</nav>
+
+
+<style>
+    
+    #contributors .page-wrapper {
+  min-height: 100vh;
+  background-color: #F5F7FA;
+  margin: auto;
+  display: flex;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  justify-content: space-evenly;
+  padding-left: calc(5% - 16px);
+  padding-right: calc(5% - 16px);
+  /* padding: 0 5%; */
+}
+#contributors nav {
+  min-height: 72px;
+  background: #FFFFFF;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.05);
+  padding: 0 5%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+#contributors .profile-box {
+  background: #FFFFFF;
+  box-shadow: 0 2px 6px 0 rgba(47, 83, 151, 0.1);
+  border-radius: 3px;
+  margin: 16px 16px;
+  padding: 16px 16px;
+  width: 200px;
+  text-align: center;
+  transition: 300ms;
+  flex-grow: 1;
+}
+#contributors .profile-box:hover {
+  /* box-shadow: 0 2px 6px 0 rgba(47,83,151,0.40); */
+  box-shadow: 0 0 20px 9px rgba(0, 0, 0, 0.03);
+}
+#contributors img {
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  box-shadow: 0 2px 6px 0 rgba(47, 83, 151, 0.1);
+}
+#contributors #logo {
+  width: 127px;
+  height: 100%;
+  border-radius: 0%;
+  background-color: white;
+  margin-right: 32px;
+  box-shadow: none;
+}
+#contributors #searchField {
+  width: 100%;
+  background: rgba(245, 247, 250, 0.2);
+  border: 1px solid #F5F7FA;
+  border-radius: 6px;
+  padding: 8px 16px;
+}
+#contributors ::placeholder {
+  color: #98A0A6;
+  font-family: 'Lato', sans-serif;
+  font-style: italic;
+  font-weight: 300;
+}
+#contributors h3 {
+  font-weight: 300;
+  font-size: 18px;
+  margin-bottom: 0;
+}
+#contributors h4 {
+  transition: 500ms;
+  font-weight: 300;
+  font-size: 14px;
+  margin-top: 8px;
+  color: #98A0A6;
+  cursor: pointer;
+}
+#contributors h4:hover {
+  color: #507BFC;
+  text-decoration: underline;
+}
+#contributors #assign,
+#contributors #view {
+  width: 50%;
+  font-size: 14px;
+  color: #98A0A6;
+  font-weight: 300;
+  padding: 8px 0;
+  border-radius: 100px;
+  transition: 300ms;
+  cursor: pointer;
+}
+#contributors #assign:hover,
+#contributors #view:hover {
+  background-color: #507BFC;
+  color: white;
+  box-shadow: 0 10px 20px 0 rgba(80, 123, 252, 0.5);
+}
+#contributors #assign {
+  float: left;
+}
+#contributors #view {
+  float: right;
+}
+#contributors i {
+  margin-right: 8px;
+}
+
+
+
+
+</style>
+
+
+<div class="hero">
+    <div class="container">
+        <div class="content">
+            <h1>যারা অবদান রেখেছেন! আমরাও করি </h1>
+            <p> লারাভেল ডকুমেন্টেশন বাংলায় অনুবাদ করে  <a href="https://github.com/laravel-bangladesh/docs" target="_blank">সহায়তা করুন</a> </p>
+        </div>
+    </div>
+</div>
+
+<section>
+
+<div id="contributors">
+  <div class="container">
+      <div class="page-wrapper" id="contributors_list"></div>
+  </div>
+</div>
+
+
+</section>
+
+@endsection
