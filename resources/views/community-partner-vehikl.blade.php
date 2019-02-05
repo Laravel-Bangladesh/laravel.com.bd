d9563afb0590213d02a6ca3f7f2c4b732f3e2d57
Jubayed Hossain <hmjubayed@gmail.com>
2019-02-06 02:39:15 +0600

    Update community-partner-vehikl.blade.php

--- resources/views/community-partner-vehikl.blade.php
+++ resources/views/community-partner-vehikl.blade.php
@@ -23,52 +23,64 @@
         <div class="partner-profile-header clearfix">
             <div class="partner-logo"><img src="/assets/svg/partner-logo-vehikl-full.svg"></div>
             <div class="partner-ctas">
-                <a href="mailto:go+partner@vehikl.com"><div class="btn btn-primary">Hire Vehikl</div></a>
-                <a href="http://vehikl.com"><div class="btn btn-default">Visit Website</div></a>
+                <a href="http://hostpresto.com"><div class="btn btn-default">Visit Website</div></a>
             </div>
         </div>
 
         <div class="row">
             <div class="partner-profile-overview col-md-6">
-                <div class="partner-profile-img"><img src="/assets/img/partner-img-vehikl.png"></div>
+                <div class="partner-profile-img"><img src="/assets/img/hostpresto-img-vehikl.png"></div>
 
                 <div class="partner-proficiencies">
-                    <h4>Proficiencies</h4>
+                    <h4>Service</h4>
                     <div class="flex">
                         <ul class="partner-proficiencies-list multi-col flex-fill">
-                            <li>Laravel</li>
-                            <li>React</li>
-                            <li>Angular</li>
-                            <li>Vue</li>
-                            <li>UI/UX Product Design</li>
-                            <li>API Design</li>
+                            <li>Web Hosting</li>
+                            <li>Wordpress Hosting</li>
+                            <li>Domain Names</li>
+                            <li>Cloud Servers</li>
+                            <li>Email Hosting</li>
+                            <li>Windows Hosting</li>
                         </ul>
 
                         <ul class="partner-proficiencies-list multi-col flex-fill">
-                            <li>Software Architecture</li>
-                            <li>Consulting</li>
-                            <li>Technical Leadership</li>
-                            <li>Staff Augmentation</li>
+                            <li>FileMaker Hosting</li>
+                            <li>Plesk Hosting</li>
+                            <li>ASP Hosting</li>
+                            <li>Node.js Hosting</li>
+                            <li>Serif WebPlus Hostin</li>
+                            <li>Cloud Servers</li>
+                            
                         </ul>
                     </div>
                 </div>
+                
+                
+
+
+
+g
+
 
                 <div class="partner-social">
                     <h4>Elsewhere</h4>
 
                     <ul class="partner-social-list">
-                        <li><a href="https://github.com/vehikl"><img src="/assets/svg/icon-github.svg"></a></li>
-                        <li><a href="https://twitter.com/vehikl"><img src="/assets/svg/icon-twitter.svg"></a></li>
-                        <li><a href="https://www.linkedin.com/company-beta/2240858/?pathWildcard=2240858"><img src="/assets/svg/icon-linkedin.svg"></a></li>
+                        <li><a href="https://github.com/hostpresto"><img src="/assets/svg/icon-github.svg"></a></li>
+                        <li><a href="https://twitter.com/hostpresto"><img src="/assets/svg/icon-twitter.svg"></a></li>
+                      
                     </ul>
                 </div>
             </div>
 
             <div class="partner-profile-bio col-md-6">
-                <h2>About Vehikl</h2>
-                <p>Vehikl is a team of code-crushing Laravel experts.  Over the years we have built a variety of web applications for customers using Laravel as our framework of choice and implemented Lean Agile development techniques to build professional applications that are functional and easy to use. </p>
-                <p>Combining the power of Laravel, Lean Agile development techniques, and a diverse team with deep technical knowledge and experience allows us to provide a unique approach to make sure your work gets done as quickly, professionally, and economically as possible.  Our customers range from smaller startups looking for help with working through a feature backlog as they scale, to larger established firms that need to refactor a legacy code base and build new features. </p>
-                <p>We integrate fully with your existing workflow and will dramatically increase your project’s velocity.  We also provide mentorship to more junior developers and work to assist knowledge transfer to any new developers you onboard internally.  As a development partner, Vehikl provides a flexible, scalable option while you ramp up an internal team.</p>
+                <h2>About hostpresto</h2>
+               
+                <p>
+                
+
+Leading provider of UK web hosting services. Shared hosting, VPS, cloud servers, reseller hosting & more!
+                </p>
             </div>
         </div>
     </div>
