d243aaef9e942585b6fc74fdd397aebce525d06e
Jubayed Hossain <hmjubayed@gmail.com>
2019-02-06 02:15:02 +0600

    Update partners.blade.php

--- resources/views/partners.blade.php
+++ resources/views/partners.blade.php
@@ -121,7 +121,7 @@
                     </a>
                 </div>
                 
-                -->
+        
 
                 <div class="flex-row">
                     <a href="mailto:&#116;&#097;&#121;&#108;&#111;&#114;&#064;&#108;&#097;&#114;&#097;&#118;&#101;&#108;&#046;&#099;&#111;&#109;" class="partner-card-md partner-card-call mb-2">
@@ -134,6 +134,8 @@
                         </div>
                     </a>
                 </div>
+                
+                        -->
             </div>
         </div>
     </section>
